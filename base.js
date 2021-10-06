80c8d8f495a512ff2b8d4c60de71cd22aa9f58b3
Zoltán Piskor <corvinus.zoltan.piskor@gmail.com>
2021-10-06 17:31:15 +0200

    DragAndDrop

--- base.js
+++ base.js
@@ -1,15 +1,65 @@
-const position = { x: 0, y: 0 };
+/* The dragging code for '.draggable' from the demo above
+ * applies to this demo as well so it doesn't have to be repeated. */
 
-interact(".draggable").draggable({
-  listeners: {
-    start(event) {
-      console.log(event.type, event.target);
-    },
-    move(event) {
-      position.x += event.dx;
-      position.y += event.dy;
+// enable draggables to be dropped into this
+interact(".dropzone").dropzone({
+  // only accept elements matching this CSS selector
+  accept: ".drag-drop",
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
 
-      event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
-    },
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add("drop-active");
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add("drop-target");
+    draggableElement.classList.add("can-drop");
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove("drop-target");
+    event.relatedTarget.classList.remove("can-drop");
+  },
+  ondrop: function (event) {
+    // event.relatedTarget.textContent = "Dropped";
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove("drop-active");
+    event.target.classList.remove("drop-target");
   },
 });
+
+interact(".drag-drop").draggable({
+  inertia: true,
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "#pageContainer",
+      endOnly: true,
+    }),
+  ],
+  autoScroll: true,
+  // dragMoveListener from the dragging demo above
+  listeners: { move: dragMoveListener },
+});
+
+function dragMoveListener(event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+  // update the position attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
